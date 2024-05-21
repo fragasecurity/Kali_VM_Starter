3be41d744dc133a1b5bdc5d6017be20f12b9b6d2
fragasecurity <117662973+fragasecurity@users.noreply.github.com>
2024-05-21 19:58:06 -0400

    Update quick_start.sh

--- quick_start.sh
+++ quick_start.sh
@@ -10,6 +10,8 @@ echo "unzipping rockyou"
 sudo gunzip /usr/share/wordlists/rockyou.txt.gz
 echo "installing rlwrap"
 sudo apt install rlwrap -y
+echo "installing dirsearch"
+sudo apt install dirsearch -y
 echo "installing gnome, please pick gdm3"
 sudo apt install kali-desktop-gnome -y
 echo "installing neo4j, remember to go to localhost:7474 and change password"
